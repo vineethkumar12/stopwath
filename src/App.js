bfb45b7001ef8d708559897716ef56d0a6011879
vineethkumar12 <adepuvineethkumarvinni@gmail.com>
2023-01-26 17:28:58 +0530

    simple stopwatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+function App() { 
+  let [seconds,minutes,hours]=[0,0,0]
+  let display=document.getElementById("display");
+  let timer=null;
+  const watch=()=>{ 
+   
+          seconds++;
+         if(seconds===60)
+         {
+               seconds=0;
+               minutes++;
+           if(minutes===60)
+           { minutes=0;
+              hours++;}
+         } 
+          let hou=hours<10? "0"+hours:hours;
+          let minu=minutes<10 ? "0"+minutes:minutes;
+          let sec=seconds<10? "0"+seconds:seconds;
+         display.innerHTML=hou+":"+ minu+":"+sec;
+         console.log("jfom")
+  } 
+
+
+
+  const startwatch=()=>{
+    if(timer!==null)
+     {clearInterval(timer)}
+    timer= setInterval(watch,1000);
+   
+
+  }
+  const stopwatch=()=>{
+    
+    clearInterval(timer)
+  
+
+
+  } 
+  const resetwatch=()=>{
+    
+    clearInterval(timer) 
+     seconds=0
+     minutes=0
+     hours=0
+    display.innerHTML="00:00:00";
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <section> 
+      {(()=>{  let posts=[]
+       for(let i=0; i<128; i++)
+       { //
+       
+      posts.push( <span key={i} className='span'></span>)
+      
+      }
+       return posts
+
+       })() 
+       }   
+          <div className='stopwatch1'>
+       <div className='stopwatch'> 
+       <h1 id="display" style={{color:"white"}}>00:00:00</h1>
+       <div className='buttons' >
+       <img src="stop.png" onClick={stopwatch} alt="noimage"/>
+       <img src="start.png" onClick={startwatch} alt="noimage" />
+       <img src="reset.png" onClick={resetwatch} alt="noimage"/>
+      
+      </div> 
+       </div> 
+       </div>
+       </section> 
     </div>
   );
 }
